5175f868feed7678f3fa116a672a1c53a1c46816
Võ Hoàng Huy <hoanghuy.dev0210@gmail.com>
2026-01-24 21:31:12 +0700

    feat(dashboard): enhance dashboard route with empty state

--- apps/web/src/routes/_protected/app/_dashboard/index/route.tsx
+++ apps/web/src/routes/_protected/app/_dashboard/index/route.tsx
@@ -1,9 +1,36 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { BellSimpleRingingIcon } from "@phosphor-icons/react";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { buttonVariants } from "@/components/ui/button";
+import {
+	Empty,
+	EmptyContent,
+	EmptyDescription,
+	EmptyHeader,
+	EmptyMedia,
+	EmptyTitle,
+} from "@/components/ui/empty";
 
 export const Route = createFileRoute("/_protected/app/_dashboard/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return null;
+	return (
+		<Empty className="min-h-[60vh]">
+			<EmptyHeader className="max-w-lg">
+				<EmptyMedia variant="icon">
+					<BellSimpleRingingIcon weight="duotone" className="size-6" />
+				</EmptyMedia>
+				<EmptyTitle>Tính năng luyện tập đã được cập bến!!!</EmptyTitle>
+				<EmptyDescription>
+					Trải nghiệm tính năng luyện tập mới ngay bây giờ.
+				</EmptyDescription>
+			</EmptyHeader>
+			<EmptyContent className="max-w-lg">
+				<Link to="/app/practices" className={buttonVariants({ size: "lg" })}>
+					Bắt đầu luyện tập
+				</Link>
+			</EmptyContent>
+		</Empty>
+	);
 }
